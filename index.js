6bd8d697c57e40789a94682fa7d006f8467bc0a3
whiplade <nmawira13@gmail.com>
2023-06-20 17:46:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+function findMatching(list, name){
+
+    return list.filter(function (driverName){
+        return driverName.toUpperCase() === name.toUpperCase();
+
+    });
+}
+
+function fuzzyMatch(list, nameBegin){
+
+    let fullName = nameBegin.length;
+    return list.filter(function(driverName){
+        return driverName.slice(0, fullName) === nameBegin;
+
+    });
+
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(list, name){
+
+    return list.filter(function(driver){
+        return driver[`name`].toLowerCase() === name.toLowerCase();
+
+    });
+  }
